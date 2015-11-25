e84b4661b0871fa63b3378591280bbfcd0dadcb5
Dmitry Morozov <dvmorozov@hotmail.com>
2015-11-25 22:22:30 +0500

    Simple arc is drawn.

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
 import xml.etree.ElementTree as ET
+import math
 
-inFileName = 'c:\\temp\\good.gpx'
-outFileName = 'c:\\temp\\gps.gpx'
+inFileName = 'e:\\temp\\good.gpx'
+outFileName = 'e:\\temp\\gps.gpx'
 
-startLat = 0.0
-startLon = 0.0
+startPoint = dict(lat=26.57, lon=67.2)
+endPoint = dict(lat=26.57, lon=139.2)
 
-stepLat = 1.0
-stepLon = 1.0
+step = 1.0
 
 ET.register_namespace('', "http://www.topografix.com/GPX/1/0")
 tree = ET.parse(inFileName)
 root = tree.getroot()
+
+
+def AddPoint(track, lon, lat):
+    point = ET.Element("{http://www.topografix.com/GPX/1/0}trkpt", dict(lon=str(lon),
+                                                                        lat=str(lat)))
+    track.append(point)
+
+    time = ET.Element("{http://www.topografix.com/GPX/1/0}time")
+    time.text = '2011-09-22T18:57:01Z'
+    point.append(time)
+
+    fix = ET.Element("{http://www.topografix.com/GPX/1/0}fix")
+    fix.text = '2d'
+    point.append(fix)
+
+    sat = ET.Element("{http://www.topografix.com/GPX/1/0}sat")
+    sat.text = '6'
+    point.append(sat)
+
+
+def DrawSegment(track, startPoint, endPoint):
+    deltaLon = endPoint['lon'] - startPoint['lon']
+    deltaLat = endPoint['lat'] - startPoint['lat']
+    angleLen = math.sqrt(deltaLon * deltaLon + deltaLat * deltaLat)
+    stepLon = deltaLon * step / angleLen
+    stepLat = deltaLat * step / angleLen
+
+    # Current values of latitude and longitude.
+    lon = startPoint['lon']
+    lat = startPoint['lat']
+
+    def AddStep(lon, lat):
+        AddPoint(track, lon, lat)
+        lon += stepLon
+        lat += stepLat
+        deltaLat1 = lat - startPoint['lat']
+        deltaLon1 = lon - startPoint['lon']
+        return math.sqrt(deltaLon1 * deltaLon1 + deltaLat1 * deltaLat1), lon, lat
+
+    len, lon, lat = AddStep(lon, lat)
+
+    while len <= angleLen:
+        len, lon, lat = AddStep(lon, lat)
+
 
 print(root.tag)
 
@@ -19,23 +63,7 @@ for trk in root.iter('{http://www.topografix.com/GPX/1/0}trkseg'):
     # Удал. т. раобтает только для дочерних.
     for point in trk.findall('{http://www.topografix.com/GPX/1/0}trkpt'):
         trk.remove(point)
-
-    for i in range(0, 10):
-        point = ET.Element("{http://www.topografix.com/GPX/1/0}trkpt", dict(lon=str(startLon + stepLon * i),
-                                                                            lat=str(startLat + stepLat * i)))
-        trk.append(point)
-
-        time = ET.Element("{http://www.topografix.com/GPX/1/0}time")
-        time.text = '2011-09-22T18:57:01Z'
-        point.append(time)
-
-        fix = ET.Element("{http://www.topografix.com/GPX/1/0}fix")
-        fix.text = '2d'
-        point.append(fix)
-
-        sat = ET.Element("{http://www.topografix.com/GPX/1/0}sat")
-        sat.text = '6'
-        point.append(sat)
+    DrawSegment(trk, startPoint, endPoint)
 
 for trk in root.iter('{http://www.topografix.com/GPX/1/0}trkpt'):
     print(trk.attrib)
