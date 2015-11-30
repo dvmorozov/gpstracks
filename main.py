1c137b93d040ab24b63e521d289b9b3126d10204
Dmitry Morozov <dvmorozov@hotmail.com>
2015-11-30 23:22:20 +0500

    New methods for math were added.

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ pointFileName = 'e:\\temp\\points.json'
 
 drawMesh = True
 
-step = 1.0
+step = 1.0  # Degrees.
 
 ET.register_namespace('', "http://www.topografix.com/GPX/1/0")
 tree = ET.parse(inFileName)
 root = tree.getroot()
 
+def CrossProduct(a, b):
+    x = a[1] * b[2] - a[2] * b[1]
+    y = a[2] * b[0] - a[0] * b[2]
+    z = a[0] * b[1] - a[1] * b[0]
+    return x, y, z
+
+
 def DegToRad(angle):
     return angle * math.pi / 180.0
+
+
+def RadToDeg(angle):
+    return angle * 180.0 / math.pi
 
 
 #  Radius is given as 1.
@@ -27,18 +38,60 @@ def SphericalToCartesian(lon, lat):
 
 
 def CartesianInnerProduct(v1, v2):
-    print('v1 = ' + str(v1[0]) + ', ' + str(v1[1]) + ', ' + str(v1[2]))
-    print('v2 = ' + str(v2[0]) + ', ' + str(v2[1]) + ', ' + str(v2[2]))
+    #print('v1 = ' + str(v1[0]) + ', ' + str(v1[1]) + ', ' + str(v1[2]))
+    #print('v2 = ' + str(v2[0]) + ', ' + str(v2[1]) + ', ' + str(v2[2]))
     return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
 
 
 def SpericalToVector(lon, lat):
     x, y, z = SphericalToCartesian(lon, lat)
-    v = []
-    v.append(x)
-    v.append(y)
-    v.append(z)
+    v = [x, y, z]
     return v
+
+
+def Sign(value):
+    return -1 if value < 0 else 1
+
+
+def VectorToSpherical(v):
+    x = v[0]
+    y = v[1]
+    z = v[2]
+    lat = math.acos(math.sqrt(x * x + y * y) / math.sqrt(x * x + y * y + z * z)) * Sign(z)
+    lon = math.acos(math.fabs(x) / math.sqrt(x * x + y * y)) * Sign(y)
+    return RadToDeg(lon), RadToDeg(lat)
+
+
+def NormalizeVector(v):
+    norma = math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
+    if norma != 0.0:
+        return [v[0] / norma, v[1] / norma, v[2] / norma]
+    else:
+        return v
+
+
+def RotateAround(axis, vector, angle):
+    # Clockwise direction.
+    phi = (-1) * DegToRad(angle)
+    matrix = {}
+    axis = NormalizeVector(axis)
+    l = axis[0]
+    m = axis[1]
+    n = axis[2]
+    matrix[0, 0] = l * l + math.cos(phi) * (1 - l * l)
+    matrix[0, 1] = l * (1 - math.cos(phi)) * m + n * math.sin(phi)
+    matrix[0, 2] = l * (1 - math.cos(phi)) * n - m * math.sin(phi)
+    matrix[1, 0] = l * (1 - math.cos(phi)) * m - n * math.sin(phi)
+    matrix[1, 1] = m * m + math.cos(phi) * (1 - m * m)
+    matrix[1, 2] = m * (1 - math.cos(phi)) * n + l * math.sin(phi)
+    matrix[2, 0] = l * (1 - math.cos(phi)) * n + m * math.sin(phi)
+    matrix[2, 1] = m * (1 - math.cos(phi)) * n - l * math.sin(phi)
+    matrix[2, 2] = n * n + math.cos(phi) * (1 - n * n)
+    vector1 = [0, 0, 0]
+    vector1[0] = matrix[0, 0] * vector[0] + matrix[0, 1] * vector[1] + matrix[0, 2] * vector[2]
+    vector1[1] = matrix[1, 0] * vector[0] + matrix[1, 1] * vector[1] + matrix[1, 2] * vector[2]
+    vector1[2] = matrix[2, 0] * vector[0] + matrix[2, 1] * vector[1] + matrix[2, 2] * vector[2]
+    return vector1
 
 
 def PointToVector(p):
@@ -49,6 +102,17 @@ def AngleBetwee2Points(p1, p2):
     cos = CartesianInnerProduct(PointToVector(p1), PointToVector(p2))
     print('cos = ' + str(cos))
     return math.acos(cos)
+
+
+def RotateStartVector(startLon, startLat, endLon, endLat):
+    x1 = SpericalToVector(startLon, startLat)
+    y1 = SpericalToVector(endLon, endLat)
+    # Rotation axis.
+    z1 = CrossProduct(x1, y1)
+    x2 = RotateAround(z1, x1, step)
+    z2 = CrossProduct(x2, y1)
+    lon, lat = VectorToSpherical(x2)
+    return CartesianInnerProduct(z2, z1), lon, lat
 
 
 def ReadData():
@@ -92,36 +156,31 @@ def AddTrack(rootElement, trackName):
     rootElement.append(track)
     return trkseg
 
+def AddStep(track, lon, lat, endLon, endLat, n):
+    print('#' + str(n) + ', lon = ' + str(lon) + ', lat = ' + str(lat))
+    AddPoint(track, lon, lat)
+    return RotateStartVector(lon, lat, endLon, endLat)
+
 
 def DrawSegment(track, startPoint, endPoint):
-    deltaLon = endPoint['lon'] - startPoint['lon']
-    deltaLat = endPoint['lat'] - startPoint['lat']
-    angleLen = math.sqrt(deltaLon * deltaLon + deltaLat * deltaLat)
-    stepLon = deltaLon * step / angleLen
-    stepLat = deltaLat * step / angleLen
-
     # Current values of latitude and longitude.
     lon = startPoint['lon']
     lat = startPoint['lat']
 
     angle = AngleBetwee2Points(startPoint, endPoint)
-    print('angle ' + str(angle * 180 / math.pi))
+    print('angle between points: ' + str(angle * 180 / math.pi))
 
-    def AddStep(lon, lat):
-        x, y, z = SphericalToCartesian(lon, lat)
-        print('x, y, z = ', str(x), ', ', str(y), ', ', str(z))
+    n = 0
+    # Start point.
+    innerProduct, lon, lat = AddStep(track, lon, lat, endPoint['lon'], endPoint['lat'], n)
+    n += 1
 
-        AddPoint(track, lon, lat)
-        lon += stepLon
-        lat += stepLat
-        deltaLat1 = lat - startPoint['lat']
-        deltaLon1 = lon - startPoint['lon']
-        return math.sqrt(deltaLon1 * deltaLon1 + deltaLat1 * deltaLat1), lon, lat
+    while innerProduct > 0:
+        innerProduct, lon, lat = AddStep(track, lon, lat, endPoint['lon'], endPoint['lat'], n)
+        n += 1
 
-    len, lon, lat = AddStep(lon, lat)
-
-    while len <= angleLen:
-        len, lon, lat = AddStep(lon, lat)
+    # End point.
+    AddStep(track, endPoint['lon'], endPoint['lat'], endPoint['lon'], endPoint['lat'], n)
 
 
 def DrawTrack(rootElement, trackData):
